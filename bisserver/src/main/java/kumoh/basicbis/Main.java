49fc42fd2951add314110879e3312abfda1af1bb
Yeon-Ung Choi <kqwe1859@gmail.com>
2021-05-05 14:38:08 +0900

    mariadb jdbc driver 추가, maven으로 옮김, main 이상하게 만든거 수정

---
+++ bisserver/src/main/java/kumoh/basicbis/Main.java
@@ -0,0 +1,21 @@
+package kumoh.basicbis;
+
+public class Main {
+
+    public static int PORT_NUMBER = 25252;
+    public static ConnectionHelper conn;
+
+    public static void main(String[] args) {
+        System.out.println("Tofu babo...");
+        System.out.println("Creative project bis server on");
+
+        if (args.length > 1) {
+            PORT_NUMBER = Integer.valueOf(args[1]);
+        }
+
+        System.out.println("Running on port " + PORT_NUMBER);
+
+        conn = new ConnectionHelper();
+        conn.startServer(PORT_NUMBER);
+    }
+}

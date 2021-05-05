20db7af479a2878cbb62b37a1b08205beec668b0
Yeon-Ung Choi <kqwe1859@gmail.com>
2021-05-05 14:23:20 +0900

    프로토콜 몇개 추가

--- bisserver/src/kumoh/basicbis/ProtocolType.java
+++ bisserver/src/kumoh/basicbis/ProtocolType.java
@@ -1,8 +1,11 @@
 package kumoh.basicbis;
 
 public enum ProtocolType {
-    UNKNOWN(0);  // =0 X ="0" "TOSS"
-    //TODO: 여기에 프로토콜 입력
+    UNKNOWN(0),  // =0 X ="0" "TOSS"
+    //TODO: 여기에 프로토콜 입력, 더 필요할시 문서에 적어주세용~
+    BUS_STOP_REQ(1), BUS_STOP_RES(2),
+    ROUTE_REQ(3), ROUTE_RES(4),
+    FOOD_REQ(5), FOOD_RES(6);
 
     private int value;
 

a7c2fb972bc3ade31e1694dd9f582c7417d02b19
HelloKS <kqwe1859@gmail.com>
2021-05-15 13:58:54 +0900

    RequestManager

---
+++ bisclient/src/main/java/kumoh/basicbis/util/RequestManager.java
@@ -0,0 +1,31 @@
+package kumoh.basicbis.prototest.util;
+
+import kumoh.basicbis.prototest.ProtocolType;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class RequestManager {
+    public String sendRequest(String csvRequest) {
+        String result = "";
+
+        try {
+            Socket sock = new Socket("127.0.0.1", 25252);
+            PrintWriter pw = new PrintWriter(sock.getOutputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+
+            pw.write(csvRequest);
+            pw.flush();
+            sock.shutdownOutput();
+
+            result = br.readLine();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
---
+++ bisclient/src/main/java/kumoh/basicbis/util/RouteTask.java
@@ -0,0 +1,7 @@
+package kumoh.basicbis.prototest.util;
+
+public class RouteTask {
+    RequestManager manager = new RequestManager();
+
+    //
+}

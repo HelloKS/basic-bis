7bbc41f342220fff37bc1f2fdc4b472ce3dc66a7
HelloKS <kqwe1859@gmail.com>
2021-05-17 17:04:11 +0900

    메인화면

--- bisclient/src/main/java/kumoh/basicbis/App.java
+++ bisclient/src/main/java/kumoh/basicbis/App.java
@@ -17,22 +17,15 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        Parent root = FXMLLoader.load(getClass().getResource("/maindisplay.fxml"));
+        scene = new Scene(root, 640, 400);
+        stage.resizableProperty().setValue(false);
         stage.setScene(scene);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }
---
+++ bisclient/src/main/java/kumoh/basicbis/MainController.java
@@ -0,0 +1,28 @@
+package kumoh.basicbis;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class MainController {
+    @FXML protected void openSearch(ActionEvent event) {
+
+    }
+
+    @FXML protected void openRT(ActionEvent event) {
+
+    }
+
+    @FXML protected void openTT(ActionEvent event) {
+
+    }
+
+    @FXML protected void openBS(ActionEvent event) {
+
+    }
+
+    @FXML protected void openFD(ActionEvent event) {
+
+    }
+}

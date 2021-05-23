69acbe190977102b9749bd6ea1a02547b7459a84
HelloKS <kqwe1859@gmail.com>
2021-05-23 17:45:17 +0900

    시간표 목록에서 보기버튼 누르면 창 띄우도록 추가

--- bisclient/src/main/java/kumoh/basicbis/ScheduleController.java
+++ bisclient/src/main/java/kumoh/basicbis/ScheduleController.java
@@ -1,13 +1,20 @@
 package kumoh.basicbis;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 import kumoh.basicbis.persistence.RouteInfo;
 import kumoh.basicbis.util.RouteTask;
+
+import java.io.IOException;
 
 public class ScheduleController {
     @FXML private TableView<RouteInfo> tableView;
@@ -35,20 +42,46 @@ public class ScheduleController {
         nameColumn.setCellValueFactory(item -> new ReadOnlyStringWrapper(item.getValue().getName()));
         nameColumn.setPrefWidth(400);
 
-        final TableColumn<RouteInfo, Void> actionColumn = new TableColumn<>("시간표");
+        final TableColumn<RouteInfo, RouteInfo> actionColumn = new TableColumn<>("시간표");
+        actionColumn.setCellValueFactory(item -> new ReadOnlyObjectWrapper<>(item.getValue()));
         actionColumn.setCellFactory(item -> new TableCell<>() {
             @Override
-            protected void updateItem(Void item, boolean empty) {
+            protected void updateItem(RouteInfo item, boolean empty) {
                 super.updateItem(item, empty);
-                if (empty) {
+                if (item == null) {
                     setGraphic(null);
                 } else {
                     Button actionButton = new Button("보기");
+                    actionButton.setOnAction(
+                            event -> handleOpenTimetableBtn(item)
+                    );
                     setGraphic(actionButton);
                 }
             }
         });
 
         return new TableColumn[]{idColumn,nameColumn,actionColumn};
+    }
+
+    public void handleOpenTimetableBtn(RouteInfo route) {
+        Parent root;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/busTimeTable.fxml"));
+            root = (Parent) loader.load();
+
+            TimetableController controller = loader.getController();
+            controller.initData(route);
+
+            Scene scene = new Scene(root);
+
+            Stage stage = new Stage();
+            stage.setTitle("시간표 확인");
+            stage.setScene(scene);
+
+            stage.resizableProperty().setValue(false);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- bisclient/src/main/java/kumoh/basicbis/TimetableController.java
+++ bisclient/src/main/java/kumoh/basicbis/TimetableController.java
@@ -1,4 +1,23 @@
 package kumoh.basicbis;
 
-public class TimetableController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import kumoh.basicbis.persistence.RouteInfo;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TimetableController implements Initializable {
+    @FXML Label name;
+    RouteInfo route;
+
+    public void initData(RouteInfo selection) {
+        route = selection;
+        name.setText(route.getId() + ": " + route.getName());
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 }

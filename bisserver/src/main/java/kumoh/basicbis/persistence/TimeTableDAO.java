7bb0caf678dccf234f811acf59bd2fb0a188414a
HelloKS <kqwe1859@gmail.com>
2021-05-30 03:39:39 +0900

    시간표 첫차/막차 DAO 수정

--- bisserver/src/main/java/kumoh/basicbis/persistence/TimeTableDAO.java
+++ bisserver/src/main/java/kumoh/basicbis/persistence/TimeTableDAO.java
@@ -48,48 +48,40 @@ public class TimeTableDAO extends BaseDAOImpl{
     }
 
     /*
-    * SQL 설명 : sql을 2번 써서 각각 버스노선UID의 !!평일!! 첫차시간과 막차시간을 가져옴
+    * SQL 설명 : 각각 버스노선UID의 !!평일!! 첫차시간과 막차시간을 가져옴
     * 사용처1 : 버스 노선 안내 화면 - 첫차시간&막차시간 출력
     * 사용처2 : 노선 검색 화면 - 결과항목 중 노선 하나 선택시, 노선 정보가 출력
     * 솔직히 제대로 작동하는지는 테스트 해봐야함
     * */
     public ArrayList<TimeTableDTO> getFirstLastTimetable(String uid){
-        String firstSql = "select * from timetable  where isholiday = 0 and tt_routeid = ?" +
-                " order by tt_starttime asc limit 1;";
-        String secondSql = "select * from timetable  where isholiday = 0 and tt_routeid = ?" +
-                " order by tt_starttime desc limit 1;";
+
+        // 가능하면 쿼리는 한번만 날리는게 어떤가요?
+        String firstSql =
+                "(SELECT * FROM timetable WHERE tt_isholiday = '0' AND tt_routeid = ? ORDER BY tt_starttime ASC LIMIT 1)" +
+                "UNION ALL" + //속도가 빨라지는 마법
+                "(SELECT * FROM timetable WHERE tt_isholiday = '0' AND tt_routeid = ? ORDER BY tt_starttime DESC LIMIT 1);";
+
         PreparedStatement statement = null;
         ResultSet resultSet = null;
         ArrayList<TimeTableDTO> list = new ArrayList<>();
-        try{
+        try {
             getConnection();
             statement = conn.prepareStatement(firstSql);
-            statement.setString(1, uid);
+            statement.setString(1, uid); //ASC
+            statement.setString(2, uid); //DESC
             resultSet = statement.executeQuery();
 
-            while(resultSet.next())
-            {
+            while(resultSet.next()) {
                 TimeTableDTO timetable = new TimeTableDTO();
                 timetable.setUid(resultSet.getString("tt_routeid"));
                 timetable.setStartTime(resultSet.getTime("tt_starttime"));
                 timetable.setHoliday(resultSet.getBoolean("tt_isHoliday"));
                 list.add(timetable);
             }
-            statement = conn.prepareStatement(secondSql);
-            statement.setString(1, uid);
-            resultSet = statement.executeQuery();
 
-            while(resultSet.next())
-            {
-                TimeTableDTO timetable = new TimeTableDTO();
-                timetable.setUid(resultSet.getString("tt_routeid"));
-                timetable.setStartTime(resultSet.getTime("tt_starttime"));
-                timetable.setHoliday(resultSet.getBoolean("tt_isHoliday"));
-                list.add(timetable);
-            }
-        }catch(SQLException se){
+        } catch(SQLException se) {
             se.printStackTrace();
-        }finally{
+        } finally {
             try{
                 if(resultSet != null) resultSet.close();
                 if(statement != null) statement.close();

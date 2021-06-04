65cde1490dff06babf6e5ca07f20fa1d22f4e320
HelloKS <kqwe1859@gmail.com>
2021-06-04 19:44:11 +0900

    정류장 정보창에서 주변 음식점 볼 수 있게 통합 및 메인메뉴 정리

--- bisclient/src/main/java/kumoh/basicbis/FoodRecomController.java
+++ bisclient/src/main/java/kumoh/basicbis/FoodRecomController.java
@@ -10,8 +10,11 @@ import javafx.scene.control.*;
 import javafx.util.Callback;
 import kumoh.basicbis.persistence.BusStopInfo;
 import kumoh.basicbis.persistence.Food;
+import kumoh.basicbis.persistence.RouteInfo;
+import kumoh.basicbis.persistence.TimeTableInfo;
 import kumoh.basicbis.util.BusStopTask3;
 import kumoh.basicbis.util.FoodTask;
+import kumoh.basicbis.util.TimetableTask;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -28,7 +31,7 @@ public class FoodRecomController implements Initializable {
     private ObservableList<BusStopInfo> list = FXCollections.observableArrayList();
     private ObservableList<Food> foodList = FXCollections.observableArrayList();
 
-    public TableColumn[] getColums() {
+    public TableColumn[] getColumns() {
         final TableColumn<Food, String> foodNameColumn = new TableColumn<>("업소명");
         foodNameColumn.setCellValueFactory(item -> {
             return new ReadOnlyStringWrapper(item.getValue().getName());
@@ -52,35 +55,12 @@ public class FoodRecomController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        BusStopTask3 task = new BusStopTask3();
-
-        Callback<ListView<BusStopInfo>, ListCell<BusStopInfo>> factory = list -> new ListCell<>() {
-            @Override
-            protected void updateItem(BusStopInfo item, boolean empty) {
-                super.updateItem(item, empty);
-                setText(empty ? "" : item.getName());
-            }
-        };
-        bsComboBox.setCellFactory(factory);
-        bsComboBox.setButtonCell(factory.call(null));
-
-        task.setOnSucceeded(workerStateEvent ->
-                {
-                    for (BusStopInfo entry : task.getValue()) {
-                        list.add(entry);
-                        bsComboBox.getItems().add(entry);
-                    }
-                }
-        );
-        Thread thread = new Thread(task, "foodinit-thread");
-        thread.setDaemon(true);
-        thread.start();
     }
 
     public void searchFood(BusStopInfo selection) {
         busStopInfo = selection;
         String query = Integer.toString(selection.getServiceId());
-        foodTableView.getColumns().setAll(getColums());
+        foodTableView.getColumns().setAll(getColumns());
 
         FoodTask task = new FoodTask(query);
         task.setOnSucceeded(workerStateEvent -> {
@@ -94,7 +74,4 @@ public class FoodRecomController implements Initializable {
         thread.start();
     }
 
-    public void searchFoodBt(ActionEvent actionEvent){
-        searchFood(bsComboBox.getValue());
-    }
 }
--- bisclient/src/main/java/kumoh/basicbis/NewBusStopInfoController.java
+++ bisclient/src/main/java/kumoh/basicbis/NewBusStopInfoController.java
@@ -7,14 +7,20 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 import javafx.util.Callback;
 import kumoh.basicbis.persistence.BusStopInfo;
+import kumoh.basicbis.persistence.RouteInfo;
 import kumoh.basicbis.util.BusStopTask;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -86,6 +92,30 @@ public class NewBusStopInfoController implements Initializable {
     public void searchStop(ActionEvent actionEvent) {
         if (!searchField.getText().isBlank()) {
             searchStop(searchField.getText());
+        }
+    }
+
+    public void openFoodRecom(ActionEvent actionEvent) {
+        Parent root;
+        try {
+            BusStopInfo busStop = stopList.getSelectionModel().getSelectedItem();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/foodRecom.fxml"));
+            root = (Parent) loader.load();
+
+            FoodRecomController controller = loader.getController();
+            controller.searchFood(busStop);
+
+            Scene scene = new Scene(root);
+
+            Stage stage = new Stage();
+            stage.setTitle("주변 음식점");
+            stage.setScene(scene);
+
+            stage.resizableProperty().setValue(false);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
